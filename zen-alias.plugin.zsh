4d3cff106e7dfe173d37c51b6557bf7a76799c70
Gokuldroid <gokulprabhu.droid@gmail.com>
2021-02-05 22:54:16 +0530

    feat: add init zen directory

---
+++ zen-alias.plugin.zsh
@@ -0,0 +1,47 @@
+function store_previous_directory() {
+  if [ -e .aliases ]; then
+    export PREVIOUS_ALIAS_DIR="$PWD"
+  fi
+}
+
+function zen_alias() {
+  if [ -n "$PREVIOUS_ALIAS_DIR" ] && ! is_subdirectory "$PWD" "$PREVIOUS_ALIAS_DIR" ; then
+    aliases="$PREVIOUS_ALIAS_DIR/.aliases"
+
+    while IFS=':' read -r key value || [ -n "$key" ]; do
+      unalias "$key" > /dev/null 2>&1
+    done < $aliases
+
+    unset PREVIOUS_ALIAS_DIR
+    echo "zen aliases unloaded"
+  fi
+
+  if [ -e .aliases ]; then
+    while IFS=':' read -r key value || [ -n "$key" ]; do
+      alias "$key"="${value##*( )}"
+    done < ".aliases"
+
+    echo "zen aliases loaded"
+  fi
+  execute_init_zen_dir()
+}
+
+function execute_init_zen_dir() {
+  alias init_zen_dir >/dev/null 2>&1 && init_zen_dir
+}
+
+function is_subdirectory() {
+  local child="$1"
+  local parent="$2"
+  if [[ "${child##${parent}}" != "$child" ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function cd() {
+  store_previous_directory
+  builtin cd "$@"
+  zen_alias
+}
